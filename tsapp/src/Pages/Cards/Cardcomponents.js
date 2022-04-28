807dc7ad8108ed86fe0dc75039cf9f00cfaab288
dipesh <dipesh@techsierra.com>
2022-04-28 16:33:07 +0530

    project added

--- tsapp/src/Pages/Cards/Cardcomponents.js
+++ tsapp/src/Pages/Cards/Cardcomponents.js
@@ -1,13 +1,13 @@
 import React, { useState, useEfect } from 'react';
-import './App.css'
+import '../../App.css'
 import axios from 'axios';
 
 const Cardcompnents =(val)=>{
   const [display, setDisplay] = useState([""]);
-}
 
 
-export const getReview = (e) => {
+
+ const getReview = (e) => {
   
   e.preventDefault()
   axios.get("http://localhost:3011/api/get").then((response) => {
@@ -15,6 +15,7 @@ export const getReview = (e) => {
     setDisplay(response.data);
   });
 };
+}
 
 export default Cardcompnents;
 // const Cardcompnents = [{ id:1, NameOfDev:"Darshana", language:"Java",Date:"Jan 2022",ExperianceYears:"2",
